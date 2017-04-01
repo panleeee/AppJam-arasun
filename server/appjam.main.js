c85b05d4b10e992b0184ebecd8c06cbbc6c59025
river-stone-jin <sukjinjjang@gmail.com>
2017-04-02 02:52:13 +0900

    fixed port number

--- server/appjam.main.js
+++ server/appjam.main.js
@@ -81,7 +81,7 @@ function pm_callback(error,res,body){
 request(pm_options, pm_callback);
 
 //서버실행
-var server=app.listen(1222,function(){
+var server=app.listen(80,function(){
   var host=server.address().address;
   var port=server.address().port;
   console.log(host+':'+port);
