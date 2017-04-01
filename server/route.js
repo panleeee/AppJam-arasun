81b66538c8255ed955bf27664800c09393e7db6e
river-stone-jin <sukjinjjang@gmail.com>
2017-04-02 02:57:46 +0900

    make ejs directory and routing

--- server/route.js
+++ server/route.js
@@ -2,9 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.use(express.static(__dirname+'/public'));
+router.set('views', path.join(__dirname, 'views'));
+router.set('view engine', 'ejs');
 
+
+출처: http://bcho.tistory.com/887 [조대협의 블로그]
 router.get('/',function(req,res){
     res.sendFile(__dirname+'/public/html/index.html');
+});
+
+router.get('/weather',function(req,res){
+    res.render('weather',{});
 });
 
 module.exports = router;
